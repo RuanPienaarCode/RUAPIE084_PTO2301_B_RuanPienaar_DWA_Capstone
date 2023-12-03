8e8707f0387fda41602a74eb0d8764ecc947a161
Ruan Pienaar <Ruan3d@gmail.com>
2023-12-03 12:01:40 +0200

    Update Login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,64 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import Input from '../components/Input';
-import Backgorund from '../components/Backgorund';
-import Navbar from '../components/Navbar';
-import Main from '../styles/main-component-style';
-import Bar from '../styles/bar-component-style';
-import { supabase } from '../supabase/SupabaseClient';
-import { FcGoogle } from 'react-icons/fc';
-import { AiOutlineGithub } from 'react-icons/ai';
-import { FaDiscord } from 'react-icons/fa';
+/**
+ * Handles the change in authentication state.
+ *
+ * @param {string} event - The event that triggered the state change.
+ * @param {object} session - The session object containing user information.
+ */
 
-export default function Login() {
-  const login = async (provider) => {
-    await supabase.auth.signInWithOAuth({
-      provider: provider,
-      redirectTo: 'http://localhost:3000/',
-    });
-  };
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
 
-  if (!localStorage.getItem('sb-iljjyaxfycermifsypsy-auth-token')) {
-    return (
-      <Bar>
-        <Navbar />
-        <Main>
-          <Backgorund>
-            <span
-              style={{
-                fontSize: 40,
-                marginBottom: 15,
-                fontWeight: 500,
-                color: '#222',
-              }}
-            >
-              Login
-            </span>
-            <Input onClick={() => login('google')}>
-              <FcGoogle
-                style={{ marginRight: 'auto' }}
-                size={30}
-                color="white"
-              />
-              <span style={{ flex: 1 }}>Sign in with Google</span>
-            </Input>
-            <Input onClick={() => login('github')}>
-              <AiOutlineGithub style={{ marginRight: 'auto' }} size={30} />
-              <span style={{ flex: 1 }}>Sign in with Github</span>
-            </Input>
-            <Input onClick={() => login('discord')}>
-              <FaDiscord
-                style={{ marginRight: 'auto' }}
-                size={30}
-                color="#5562EA"
-              />
-              <span style={{ flex: 1 }}>Sign in with Discord</span>
-            </Input>
-          </Backgorund>
-        </Main>
-      </Bar>
-    );
-  }
-  if (localStorage.getItem('sb-iljjyaxfycermifsypsy-auth-token')) {
-    <div>{(window.location.href = '/')}</div>;
-  }
+import getUserData from '../components/getUserData';
+
+import getSupabase from '../assets/api';
+
+import logo from '../assets/bolt.png';
+
+const supabase = getSupabase();
+
+export default function Login(props) {
+  const { setUser } = props;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleAuthStateChange = (event, session) => {
+      console.log('LOGIN log Auth state changed:', event, session);
+
+      if (event === 'SIGNED_IN') {
+        setUser(session.user);
+        navigate('/home');
+        console.log('User signed in');
+      } else if (event === 'SIGNED_OUT') {
+        navigate('/login');
+        console.log('User signed out');
+      }
+    };
+
+    // Add the auth state change listener
+    supabase.auth.onAuthStateChange(handleAuthStateChange);
+
+    // Clean up all auth state change listeners when the component unmounts
+    return () => {
+      supabase.auth.onAuthStateChange(null);
+    };
+  }, []);
+
+  useEffect(() => {
+    getUserData(setUser);
+  }, []);
+
+  return (
+    <div className="login-page">
+      <img
+        className="logo"
+        src={logo}
+        alt="Bolt Logo"
+        style={{ maxWidth: '100%', height: 'auto' }}
+      />
+      <Auth
+        supabaseClient={supabase}
+        providers={['discord', 'github']}
+        appearance={{ theme: ThemeSupa }}
+        theme="dark"
+        socialLayout="horizontal"
+        socialButtonSize="xlarge"
+      />
+      <button onClick={() => navigate('/success')} aria-label="Go to Success">
+        Go to Success
+      </button>
+    </div>
+  );
 }
