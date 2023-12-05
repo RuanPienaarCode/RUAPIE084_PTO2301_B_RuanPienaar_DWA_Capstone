7f2f69a26f3de313f8d2cf412912426c77fde7c9
Ruan Pienaar <Ruan3d@gmail.com>
2023-12-05 17:05:14 +0200

    Update modal.jsx

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,4 +1,6 @@
-import * as React from 'react';
+// BasicModal.jsx
+
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -14,36 +16,69 @@ const style = {
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
-  color: "black"
+  color: 'black',
 };
 
 export default function BasicModal(props) {
+  const { modalOpen, setModalOpen, modalData, setModalData, showId } = props;
+  const [open, setOpen] = useState(false);
 
+  const handleOpen = async () => {
+    try {
+      const response = await fetch(
+        `https://podcast-api.netlify.app/id/${showId}`
+      );
+      const data = await response.json();
 
+      setModalData(data);
+      setOpen(true);
+    } catch (error) {
+      console.error('Error fetching podcast data:', error);
+    }
+  };
 
-  const { modalOpen, setModalOpen, modalData, setModalData } = props;
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  useEffect(() => {
+    if (showId) {
+      handleOpen();
+    }
+  }, [showId]);
+
   return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            TEXT IN THE MDOEL 
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+    <Modal
+      open={open || modalOpen}
+      onClose={() => {
+        setModalOpen(false);
+        handleClose();
+      }}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          {modalData?.title}
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          {modalData?.description}
+        </Typography>
+        {modalData?.seasons.map((season) => (
+          <div key={season.season}>
+            <Typography variant="h6" sx={{ mt: 2 }}>
+              {season.title}
+            </Typography>
+            {season.episodes.map((episode) => (
+              <div key={episode.title}>
+                <Typography sx={{ mt: 1 }}>{episode.title}</Typography>
+                <Typography sx={{ mt: 1 }}>{episode.description}</Typography>
+                <Button variant="contained" href={episode.file} sx={{ mt: 1 }}>
+                  Listen
+                </Button>
+              </div>
+            ))}
+          </div>
+        ))}
+      </Box>
+    </Modal>
   );
 }
