1bffc28fa2de0e2fb0dcce5dcfeaea0f46755cfc
Ruan Pienaar <Ruan3d@gmail.com>
2023-12-12 15:17:58 +0200

    Update PodcastCard.jsx

--- src/components/PodcastCard.jsx
+++ src/components/PodcastCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import GenreCard from './GenreCard';
-import Modal from './Modal';
+import Modal from './modal';
 import PodcastModalContent from './PodcastModalContent';
 
 /**
